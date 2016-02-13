e88e75ac3df2b9ecb776e90e44009abbc1291ea9
diginc <adam@diginc.us>
2016-02-13 16:11:04 -0600

    updating to the latest upstream pihole code

--- alpine/start.sh
+++ alpine/start.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-gravity.sh # pi-hole version minus the service dnsmasq start
-
 dnsmasq --test || exit 1
-dnsmasq
-
 php-fpm -t || exit 1
-php-fpm
-
 nginx -t || exit 1
+
+gravity.sh # pi-hole version minus the service dnsmasq start
+dnsmasq
+php-fpm
 nginx
 
 tail -F /var/log/nginx/*.log /var/log/pihole.log
--- debian/start.sh
+++ debian/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
+dnsmasq --test || exit 1
+lighttpd -t -f /etc/lighttpd/lighttpd.conf || exit 1
 
-
-service lighttpd start
 gravity.sh # dnsmasq start included
+service lighttpd start
 
 tail -f /var/log/lighttpd/*.log /var/log/pihole.log
-
