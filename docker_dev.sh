b0011d0a4603d75798e69b8aab3b3a84eddc3365
diginc <adam@diginc.us>
2016-04-25 23:18:41 -0500

    Updated version, support for custom DNS server override env vars

---
+++ docker_dev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+docker build -f alpine.docker -t dockerpihole_alpine .
+docker build -f debian.docker -t dockerpihole_debian .
+
+IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+
+docker run -it --rm --cap-add=NET_ADMIN \
+  -p 5053:53/tcp \
+  -p 5053:53/udp \
+  -p 5080:80 \
+  -e ServerIP="$IP" \
+  $@ \
+  dockerpihole_${image:-alpine}
--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-
+image=${1:-'diginc/pihole:alpine'}
 IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-docker run -p 53:53/tcp -p 53:53/udp -p 80:80 --cap-add=NET_ADMIN -e piholeIP="$IP" --name pihole -d dockerhole_alpine
+
+# Default ports + daemonized docker container
+docker run -p 53:53/tcp -p 53:53/udp -p 80:80 \
+  --cap-add=NET_ADMIN \
+  -e piholeIP="$IP" \
+  --name pihole \
+  -d $image
