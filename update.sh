ad6d4d79d0a32009f04bc8b56ce978e89f660a68
diginc <adam@diginc.us>
2016-05-24 21:55:36 -0500

    fixed crontab script

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # Grab newest code and update version files
 git submodule foreach git pull origin master;
@@ -12,7 +12,8 @@ sed -i 's|service dnsmasq start|dnsmasq -7 /etc/dnsmasq.d|g' alpine/gravity.sh
 # Copy latest crontab and modify to use docker exec commands
 cron='./docker-pi-hole.cron'
 cp -f pi-hole/advanced/pihole.cron ${cron};
-sed -i '/Update the ad sources/ i\# Your container name goes here:\nDOCKER_NAME=pihole\n' ${cron};
-sed -i 's|/usr/local/bin/|docker exec -it $DOCKER_NAME |g' ${cron};
+sed -i '/Update the ad sources/ i\# Your container name goes here:\nDOCKER_NAME=pihole\nPATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin\n' ${cron};
+sed -i 's|/usr/local/bin/|docker exec $DOCKER_NAME |g' ${cron};
+sed -i '/docker exec/ s|$| > /dev/null|g' ${cron};
 # docker-pi-hole users update their docker images, not git code
 sed -i '/pihole updateDashboard/ s/^/#/' ${cron};
