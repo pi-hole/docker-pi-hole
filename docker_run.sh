8d90235580707ad39781be30efc9fafe4dbd5a85
Adam Hill <adam@diginc.us>
2017-01-09 12:04:32 -0600

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 IMAGE=${1:-'diginc/pi-hole:alpine'}
-IP=$(ip route get 8.8.8.8 | awk '{ print $NF; exit }')
+IP_LOOKUP="$(ip route get 8.8.8.8 | awk '{ print $NF; exit }')"  # May not work for VPN / tun0
+IP="${IP:-$IP_LOOKUP}"  # use $IP, if set, otherwise IP_LOOKUP
 
 # Default ports + daemonized docker container
 docker run -p 53:53/tcp -p 53:53/udp -p 80:80 \
