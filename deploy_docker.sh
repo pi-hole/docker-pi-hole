5cc28d769a37fdc2691fff40a9040e2e9c40e7e0
Adam Hill <adam@diginc.us>
2018-08-04 19:33:45 -0500

    Updated deploy script (still needs work), fixed dead link
    Signed-off-by: Adam Hill <adam@diginc.us>

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -16,6 +16,7 @@ remoteimg="$namespace/$localimg"
 branch="$(parse_git_branch)"
 version="${version:-unset}"
 dry="${dry}"
+latest="${latest:-false}" # true as shell env var to deploy latest
 
 if [[ -n "$dry" ]]; then dry='echo '; fi
 
@@ -24,7 +25,7 @@ if [[ "$version" == 'unset' ]]; then
         echo "Version number var is unset and master branch needs a version...pass in \$version variable!"
         exit 1
     elif [[ "$branch" = "release/"* ]]; then
-        version="$(echo $branch | grep -Po 'v[\d.-]')"
+        version="$(echo $branch | grep -Po 'v[\d\.-]*')"
         echo "Version number is being taken from this release branch $version"
     else
         version="$branch"
@@ -39,15 +40,18 @@ echo "branch: $branch"
 [[ -n "$dry" ]] && echo "DRY RUN: $dry"
 echo "Example tagging: docker tag $localimg:$tag $remoteimg:${version}_amd64"
 
-$dry ./Dockerfile.py
+$dry ./Dockerfile.py --arch=amd64 --arch=armhf --arch=aarch64
 
 # ARMv6/armel doesn't have a FTL binary for v4.0 pi-hole 
 # for tag in debian_armhf debian_aarch64 debian_armel; do 
 for tag in amd64 armhf aarch64; do 
     # Verison specific tags for ongoing history
-    $dry docker tag $localimg:$tag $remoteimg:${version}_${tag} 
-    $dry docker push pihole/pihole-multiarch:${version}_${tag} 
-    # Floating latest tags 
-    $dry docker tag pi-hole-multiarch:$tag pihole/pihole-multiarch:${tag} 
-    $dry docker push pihole/pihole-multiarch:${tag} 
+    $dry docker tag $localimg:v4.0_$tag $remoteimg:${version}_${tag} 
+    $dry docker push pihole/pihole:${version}_${tag} 
+    # Floating latest tags (Conditionalize these to master?)
+    if [[ "$tag" == 'amd64' ]] && [[ "$branch" == 'master' || "$latest" == 'true' ]] ; then
+        # Latest tag should become a manifest for multiple architectures, not just amd64!
+        $dry docker tag pi-hole:v4.0_amd64 pihole/pihole:latest
+        $dry docker push pihole/pihole:latest
+    fi;
 done
