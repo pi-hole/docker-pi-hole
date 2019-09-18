a6e70b653dcb514dbb071ca488fa36aa4870c7a4
Adam Hill <adam@diginc.us>
2019-09-17 22:13:12 -0500

    Upgrade

--- install.sh
+++ install.sh
@@ -47,19 +47,21 @@ distro_check
 apt-get -y install debconf-utils
 echo resolvconf resolvconf/linkify-resolvconf boolean false | debconf-set-selections
 
-# Tried this - unattended causes starting services during a build, should probably PR a flag to shut that off and switch to that 
-#bash -ex "./${PIHOLE_INSTALL}" --unattended
-install_dependent_packages INSTALLER_DEPS[@]
-install_dependent_packages PIHOLE_DEPS[@]
-install_dependent_packages PIHOLE_WEB_DEPS[@]
+ln -s /bin/true /usr/local/bin/service
+bash -ex "./${PIHOLE_INSTALL}" --unattended
+rm /usr/local/bin/service
+# Old way of setting up
+#install_dependent_packages INSTALLER_DEPS[@]
+#install_dependent_packages PIHOLE_DEPS[@]
+#install_dependent_packages PIHOLE_WEB_DEPS[@]
 # IPv6 support for nc openbsd better than traditional
 apt-get install -y --force-yes netcat-openbsd
 
 piholeGitUrl="${piholeGitUrl}"
 webInterfaceGitUrl="${webInterfaceGitUrl}"
 webInterfaceDir="${webInterfaceDir}"
-git clone --branch "${CORE_VERSION}" --depth 1 "${piholeGitUrl}" "${PI_HOLE_LOCAL_REPO}"
-git clone --branch "${WEB_VERSION}" --depth 1 "${webInterfaceGitUrl}" "${webInterfaceDir}"
+#git clone --branch "${CORE_VERSION}" --depth 1 "${piholeGitUrl}" "${PI_HOLE_LOCAL_REPO}"
+#git clone --branch "${WEB_VERSION}" --depth 1 "${webInterfaceGitUrl}" "${webInterfaceDir}"
 
 tmpLog="/tmp/pihole-install.log"
 installLogLoc="${installLogLoc}"
