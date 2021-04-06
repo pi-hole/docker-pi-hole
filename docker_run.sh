476c8ece0417109df5aa26f1038015507b219880
batikas9090 <82052689+batikas9090@users.noreply.github.com>
2021-04-07 01:34:55 +0300

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -11,7 +11,7 @@ docker run -d \
     -p 53:53/tcp -p 53:53/udp \
     -p 80:80 \
     -p 443:443 \
-    -e TZ="America/Chicago" \
+    -e TZ="Europe/Athens" \
     -v "${PIHOLE_BASE}/etc-pihole/:/etc/pihole/" \
     -v "${PIHOLE_BASE}/etc-dnsmasq.d/:/etc/dnsmasq.d/" \
     --dns=127.0.0.1 --dns=1.1.1.1 \
