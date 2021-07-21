70c573b69eef05e26c829f1abc957ce16c961f12
Adam Warner <me@adamwarner.co.uk>
2021-07-21 19:41:35 +0100

    Ignore SKIPGRAVITYONBOOT if not gravity.db exists, which will be the case if a fresh container is built with a new (empty) volume mapped to it.
    Signed-off-by: Adam Warner <me@adamwarner.co.uk>

--- s6/debian-root/etc/cont-init.d/20-start.sh
+++ s6/debian-root/etc/cont-init.d/20-start.sh
@@ -14,7 +14,12 @@ $bashCmd /start.sh
 if [ -n "$PYTEST" ]; then
     sed -i 's/^gravity_spinup$/#gravity_spinup # DISABLED FOR PYTEST/g' "$(which gravity.sh)"
 fi
-if [ -z "$SKIPGRAVITYONBOOT" ]; then
+if [ -z "$SKIPGRAVITYONBOOT" ] || [ ! -f /etc/pihole/gravity.db ]; then
+    if [ -n "$SKIPGRAVITYONBOOT" ];then
+        echo "  SKIPGRAVITYONBOOT is set, however no gravity.db exists (Likely due to a fresh volume). This is a required file for Pi-hole to operate."
+        echo "  Ignoring SKIPGRAVITYONBOOT on this occaision."
+    fi
+
     echo '@reboot root PATH="$PATH:/usr/sbin:/usr/local/bin/" pihole updateGravity >/var/log/pihole_updateGravity.log || cat /var/log/pihole_updateGravity.log' > /etc/cron.d/gravity-on-boot
 else
     echo "  Skipping Gravity Database Update."
