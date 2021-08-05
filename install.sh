88518b036d897688bc90b389a6e915ead71c9067
Adam Warner <me@adamwarner.co.uk>
2021-08-05 20:21:35 +0100

    Inject the container tag into the web interface footer
    Signed-off-by: Adam Warner <me@adamwarner.co.uk>

--- install.sh
+++ install.sh
@@ -91,6 +91,9 @@ sed -i $'s/)\s*uninstallFunc/) unsupportedFunc/g' /usr/local/bin/pihole
 # Inject a message into the debug scripts Operating System section to indicate that the debug log comes from a Docker system.
 sed -i $'s/echo_current_diagnostic "Operating system"/echo_current_diagnostic "Operating system"\\\n    log_write "${INFO} Pi-hole Docker Container: ${PIHOLE_TAG:-PIHOLE_TAG is unset}"/g' /opt/pihole/piholeDebug.sh
 
+# Inject container tag into web interface footer...
+sed -i $"s/<ul class=\"list-unstyled\">/<strong>Docker Tag<\/strong> ${PIHOLE_TAG//\//\\/}\\n<ul class=\"list-unstyled\">/g" /var/www/html/admin/scripts/pi-hole/php/footer.php
+
 touch /.piholeFirstBoot
 
 echo 'Docker install successful'
