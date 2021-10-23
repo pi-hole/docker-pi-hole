ad4515745e5d09da9fc871e53e4093e041b26a57
Adam Warner <me@adamwarner.co.uk>
2021-10-23 13:56:36 +0100

    Add PIHOLE_VERSION as PIHOLE_DOCKER_TAG in the php environment instead of injecting the version into the admin page's footer
    Signed-off-by: Adam Warner <me@adamwarner.co.uk>

--- install.sh
+++ install.sh
@@ -88,10 +88,6 @@ sed -i $'s/)\s*uninstallFunc/) unsupportedFunc/g' /usr/local/bin/pihole
 # Inject a message into the debug scripts Operating System section to indicate that the debug log comes from a Docker system.
 sed -i $'s/echo_current_diagnostic "Operating system"/echo_current_diagnostic "Operating system"\\\n    log_write "${INFO} Pi-hole Docker Container: ${PIHOLE_VERSION:-PIHOLE_VERSION is unset}"/g' /opt/pihole/piholeDebug.sh
 
-# Inject container tag into web interface footer...
-sed -i $"s/<ul class=\"list-unstyled\">/<ul class=\"list-unstyled\">\\n<strong><li>Docker Tag<\/strong> ${PIHOLE_VERSION}<\/li>/g" /var/www/html/admin/scripts/pi-hole/php/footer.php
-sed -i $"s/<ul class=\"list-inline\">/<strong>Docker Tag<\/strong> ${PIHOLE_VERSION}\\n<ul class=\"list-inline\">/g" /var/www/html/admin/scripts/pi-hole/php/footer.php
-
 touch /.piholeFirstBoot
 
 echo 'Docker install successful'
