13f7b55438371f5e11bcd4b5060c766f0903c9fc
Phill Kelley <pmk.57t49@lgosys.com>
2021-12-10 10:50:06 +1100

    Trailing slashes on volumes fail on newer versions of docker-compose
    See [Issue 947](https://github.com/pi-hole/docker-pi-hole/issues/947)
    for the background.
    
    This PR proposes removing trailing slashes from all documentation,
    examples and scripts.

--- docker_run.sh
+++ docker_run.sh
@@ -11,8 +11,8 @@ docker run -d \
     -p 53:53/tcp -p 53:53/udp \
     -p 80:80 \
     -e TZ="America/Chicago" \
-    -v "${PIHOLE_BASE}/etc-pihole/:/etc/pihole/" \
-    -v "${PIHOLE_BASE}/etc-dnsmasq.d/:/etc/dnsmasq.d/" \
+    -v "${PIHOLE_BASE}/etc-pihole:/etc/pihole" \
+    -v "${PIHOLE_BASE}/etc-dnsmasq.d:/etc/dnsmasq.d" \
     --dns=127.0.0.1 --dns=1.1.1.1 \
     --restart=unless-stopped \
     --hostname pi.hole \
