473117e8a8c1366061b144df661e6c8c8bd68ca8
Adam Warner <me@adamwarner.co.uk>
2022-08-29 11:50:19 +0100

    Rename start and gravity scripts to match their service names
    Signed-off-by: Adam Warner <me@adamwarner.co.uk>

--- test/tests/conftest.py
+++ test/tests/conftest.py
@@ -185,7 +185,7 @@ def dig():
     return _dig
 
 '''
-Persistent Docker container for testing service post start.sh
+Persistent Docker container for testing service post _startup.sh
 '''
 @pytest.fixture
 def running_pihole(docker_persist, slow, persist_webserver):
