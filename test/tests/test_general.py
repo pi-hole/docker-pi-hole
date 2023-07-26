b56b8d03c4a6cbea284a41fcf147d39ad04260d5
Adam Warner <me@adamwarner.co.uk>
2023-07-26 22:35:21 +0100

    black formatted some tests
    Signed-off-by: Adam Warner <me@adamwarner.co.uk>

--- test/tests/test_general.py
+++ test/tests/test_general.py
@@ -1,11 +1,13 @@
 import pytest
 
+
 @pytest.mark.parametrize("test_args", ['-e "PIHOLE_UID=456"'])
 def test_pihole_uid_env_var(docker):
-    func = docker.run('id -u pihole')
+    func = docker.run("id -u pihole")
     assert "456" in func.stdout
+
 
 @pytest.mark.parametrize("test_args", ['-e "PIHOLE_GID=456"'])
 def test_pihole_gid_env_var(docker):
-    func = docker.run('id -g pihole')
+    func = docker.run("id -g pihole")
     assert "456" in func.stdout
