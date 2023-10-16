55d0ef5fa1bd0cd2d083be99624b49e72b764c28
Adam Warner <me@adamwarner.co.uk>
2023-10-16 18:00:31 +0100

    Fix tests
    Signed-off-by: Adam Warner <me@adamwarner.co.uk>

--- test/tests/test_bash_functions.py
+++ test/tests/test_bash_functions.py
@@ -5,14 +5,14 @@ import re
 CMD_APPLY_FTL_CONFIG_FROM_ENV = ". bash_functions.sh ; apply_FTL_Configs_From_Env"
 
 
-@pytest.mark.parametrize("test_args", ['-e "FTLCONF_webserver_port=999"'])
+@pytest.mark.parametrize("test_args", ['-e "FTLCONF_webserver.port=999"'])
 def test_ftlconf_webserver_port(docker):
     func = docker.run(CMD_APPLY_FTL_CONFIG_FROM_ENV)
     assert "Applied pihole-FTL setting webserver.port=999" in func.stdout
 
 
 @pytest.mark.parametrize(
-    "test_args", ['-e "FTLCONF_dns_upstreams=1.1.1.1;8.8.8.8#1234"']
+    "test_args", ['-e "FTLCONF_dns.upstreams=1.1.1.1;8.8.8.8#1234"']
 )
 def test_ftlconf_dns_upstreams(docker):
     func = docker.run(CMD_APPLY_FTL_CONFIG_FROM_ENV)
