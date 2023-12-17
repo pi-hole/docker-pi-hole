34345f21f0b60a3c3c99be9d790e6ebef75402df
richsulpz <153342250+richsulpz@users.noreply.github.com>
2023-12-17 02:50:40 -0700

    Update docker_run.sh
    Signed-off-by: richsulpz <153342250+richsulpz@users.noreply.github.com>

--- examples/docker_run.sh
+++ examples/docker_run.sh
@@ -10,7 +10,7 @@ docker run -d \
     --name pihole \
     -p 53:53/tcp -p 53:53/udp \
     -p 80:80 \
-    -e TZ="America/Chicago" \
+    -e TZ="America/Phoenix" \
     -v "${PIHOLE_BASE}/etc-pihole:/etc/pihole" \
     -v "${PIHOLE_BASE}/etc-dnsmasq.d:/etc/dnsmasq.d" \
     --dns=127.0.0.1 --dns=1.1.1.1 \
