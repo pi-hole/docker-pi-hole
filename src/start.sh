d3ab97d7b14bb2c2fc540c5ba21fc76367445dbe
yubiuser <github@yubiuser.dev>
2025-09-27 14:49:19 +0200

    Remove $PYTEST
    Signed-off-by: yubiuser <github@yubiuser.dev>

--- src/start.sh
+++ src/start.sh
@@ -146,12 +146,6 @@ stop() {
     echo "      https://docs.docker.com/engine/containers/start-containers-automatically/#use-a-restart-policy"
     echo ""
 
-    # If we are running pytest, keep the container alive for a little longer
-    # to allow the tests to complete
-    if [[ ${PYTEST} ]]; then
-        sleep 10
-    fi
-
     exit "${FTL_EXIT_CODE}"
 
 }
